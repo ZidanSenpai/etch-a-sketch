0a8c199b4a0a147780d325c602f01d630749276a
ZidanSenpai <crazyzdan786@gmail.com>
2025-06-25 08:51:47 +0530

    add a 16x16 grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.getElementById("container");
+for(let i = 1; i<= 16*16; i++){
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    div.style.width = `${640/16}px`;
+    div.style.height = `${640/16}px`;
+    container.appendChild(div);
+}
